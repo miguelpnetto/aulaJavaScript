a4330a9879c23767254b66212a78f05d1875c3c4
miguelpnetto <jmiguelpnetto@gmail.com>
2026-02-05 11:30:40 -0300

    Adicionando um While dentro do arquivo teste.js

--- teste.js
+++ teste.js
@@ -1,2 +1,13 @@
-let nome = prompt("Informe seu nome: ")
+let pessoas = []
+let sair = false
+
+while (sair) {
+    let nome = prompt("Informe seu nome: ")
+    if (nome.toLowerCase !== "sair") {
+        pessoas.push(nome)
+    } else {
+        alert(`Pessoas cadastradas:\n${pessoas}`)
+        sair = true
+    }
+}
 
