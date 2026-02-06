701f51ed7b32c741fe1e5915e10d38b68e966a77
miguelpnetto <jmiguelpnetto@gmail.com>
2026-02-06 10:31:15 -0300

    Ajustando a primeira questao

--- listaDeExercicios01/portariaDeEvento.js
+++ listaDeExercicios01/portariaDeEvento.js
@@ -1,17 +1,13 @@
 let listaVIP = ['Maria', 'João', 'Ana', 'Pedro', 'Carla']
 let pistaComum = "PISTA123"
-let sair = false
 
-while (!sair) {
     let nome = prompt("Digite seu nome:")
     let idade = Number(prompt("Digite sua idade:"))
 
     if (idade < 18) {
         alert(`Acesso negado, ${nome}. Evento restrito apenas para maiores de 18 anos.`)
-        sair = true
     } else if (listaVIP.includes(nome)) {
         alert(`Bem-vindo(a) à Área VIP, ${nome}!`)
-        sair = true
     } else {
         let codigoAcesso = prompt(`Olá, ${nome}. Seu nome não está na lista VIP. Para acessar a pista comum, insira o codigo de acesso:`)
         let tentativas = 0
@@ -19,16 +15,13 @@ while (!sair) {
         do {
             if (codigoAcesso === pistaComum) {
                 alert(`Código correto! Bem-vindo(a) à Pista Comum ${nome}!`)
-                sair = true
             } else {
                 tentativas++
                 if (tentativas < 3) {
                     codigoAcesso = prompt(`Código incorreto. Você tem mais ${3 - tentativas} tentativa(s). Insira o código de acesso novamente:`)
                 } else {
                     alert("Você excedeu o número máximo de tentativas. Acesso negado.")
-                    sair = true
                 }
             }
         } while (codigoAcesso !== pistaComum && tentativas < 3)
     }
-}
